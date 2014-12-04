b2def9b5506fc4ff3db6c92fad201cb2e210a5cd
matthew9012 <mattgia3@gmail.com>
2014-12-03 19:26:34 -0500

    Restructuring
    It still works.. yay

---
+++ app/src/main/java/com/eecswork/matthew/nagg/Fragment_Zero.java
@@ -0,0 +1,47 @@
+package com.eecswork.matthew.nagg;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * Created by Matthew on 12/3/2014.
+ */
+public class Fragment_Zero extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static Fragment_Zero newInstance(int sectionNumber) {
+        Fragment_Zero fragment = new Fragment_Zero();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public Fragment_Zero() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_zero, container, false);
+
+
+        //View v = this.getView();
+        //Button b = (Button) v.findViewById(R.id.addButton);
+
+        return rootView;
+    }
+}
--- app/src/main/java/com/eecswork/matthew/nagg/SecondActivity.java
+++ app/src/main/java/com/eecswork/matthew/nagg/SecondActivity.java
@@ -16,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -195,95 +196,6 @@ public class SecondActivity extends Activity implements ActionBar.TabListener {
         }
     }
 
-    public static class Fragment_Zero extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static Fragment_Zero newInstance(int sectionNumber) {
-            Fragment_Zero fragment = new Fragment_Zero();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public Fragment_Zero() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_zero, container, false);
-            return rootView;
-        }
-    }
-
-    public static class Fragment_One extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static Fragment_One newInstance(int sectionNumber) {
-            Fragment_One fragment = new Fragment_One();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public Fragment_One() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_one, container, false);
-            return rootView;
-        }
-    }
-
-    public static class Fragment_Two extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static Fragment_Two newInstance(int sectionNumber) {
-            Fragment_Two fragment = new Fragment_Two();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public Fragment_Two() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_two, container, false);
-            return rootView;
-        }
-    }
 
 
 
